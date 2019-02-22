98fd508db9049886a0d6b0de403c3fb41e148d6a
Leyla <lisana5632@gmail.com>
2019-02-22 22:08:36 +0300

    Added state and onChange

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,13 +1,31 @@
-import React from 'react';
-const Input=(props)=>{
-  return (
-      <div className="Input">
-          <input  id={props.name}
-                  autoComplete="false"
-                  required type={props.type}
-                  placeholder={props.placeholder} />
-          <label htmlFor={props.name}></label>
-      </div>
-  );
-}
+import React, { Component } from "react";
+
+class Input extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      name:'',
+      value:''
+    };
+    this.props=props;
+  }
+  onChange = (e) => {
+      this.setState({name:e.target.name,value: e.target.value },
+                    ()=> this.props.setValues(this.state.name,this.state.value));
+  }
+
+  render() {
+    return (
+    <div className="Input">
+        <input  id={this.props.name}
+                name={this.props.name}
+                required type={this.props.type}
+                placeholder={this.props.placeholder}
+                onChange={this.onChange}
+                />
+              <label htmlFor={this.props.name}></label>
+    </div>
+  )};
+
+};
 export default Input;
