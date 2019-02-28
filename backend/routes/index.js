1bdb374391f159f9feeb38398a3f202cd8fc50d8
Leyla <lisana5632@gmail.com>
2019-02-28 19:53:48 +0300

    added controller for getting all posts

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,9 +5,11 @@ const router = express.Router();
 
 const RegistrationController=require('../controllers/RegistrationController');
 const BlogPostController=require('../controllers/BlogPostController');
-
+const AllBlogPostsController=require('../controllers/AllBlogPostsController');
 
 router.post('/',RegistrationController.validate(),RegistrationController.createRegistration);
 router.post('/blog-post', BlogPostController.validate(),BlogPostController.createBlogPost);
+router.get('/get-blog-posts', AllBlogPostsController.show);
+
 
 module.exports = router;
